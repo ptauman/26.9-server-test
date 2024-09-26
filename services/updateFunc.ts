122405bca654d855268d58abcc13fc91509b5327
pinch <pinchasmt@gmail.com>
2024-09-26 14:18:07 +0300

    finish the break

--- services/updateFunc.ts
+++ services/updateFunc.ts
@@ -3,19 +3,17 @@ const locationPoints: number[] = []
 
 
 export async function changeStatus(beeper: dal.Beeper, data: any):Promise<dal.Beeper|string>{
-    const answer :string  = "sorry, sum thing doesn't work"
-    return beeper
+    switch (beeper.status) {
+        case dal.Status.manufactured: 
+            { beeper.status = dal.Status.assembled; break}
+        case dal.Status.assembled:
+            { beeper.status = dal.Status.shipped; break}
+        case dal.Status.shipped: 
+          
+                { beeper.status = dal.Status.deployed};
+            break}
+        case dal.Status.deployed:
+            { beeper.status = dal.Status.detonated; break}
+        default: {break }
+    }
 }
-// switch (beeper.status) {
-//     case dal.Status.manufactured: 
-//         { beeper.status = dal.Status.assembled; break}
-//     case dal.Status.assembled:
-//         { beeper.status = dal.Status.shipped; break}
-//     case dal.Status.shipped: 
-//         {if(await deployedFunc.deployedValid(beeper.status, data))
-//             { beeper.status = dal.Status.deployed};
-//         break}
-//     case dal.Status.deployed:
-//         { beeper.status = dal.Status.detonated; break}
-//     default: {break }
-// }
